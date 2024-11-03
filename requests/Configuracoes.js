b54e3b1acd5cb9c9ae94eb1a9cc5e9160679c3b4
cbcaetanoborges@gmail.com <cbcaetanoborges@gmail.com>
2024-11-03 07:22:03 +0100

    234

--- requests/Configuracoes.js
+++ requests/Configuracoes.js
@@ -6,27 +6,127 @@ class ConfiguracoesReq {
         this.notificacao = notificacao;
     }
     init() {
- 
-        
 
-        $('[name="opcao"]').change(function () { 
+
+
+        $('[name="opcao"]').change(function () {
             var opcao = $('[name="opcao"]:checked').val();
             console.log(opcao);
-            $('.opcao').css({"color":"black"});
-            $('.'+opcao).css({"color":"#640564"});
+            $('.opcao').css({ "color": "black" });
+            $('.' + opcao).css({ "color": "#640564" });
         })
         var settings = {
-        "url": (this.apiUrl)+"/config/timeout",
-        "method": "GET",
-        "timeout": 0,
-        "headers": {
-            "token": db.getToken()
-        },
+            "url": (this.apiUrl) + "/config/timeout",
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+                "token": db.getToken()
+            },
         };
 
         $.ajax(settings).done(function (response) {
-             $('.'+response.payload+"s").css({"color":"#640564"});
+            console.log(response);
+            $('.' + response.payload).css({ "color": "#640564" });
         });
 
+    }
+
+    alterarTempo() {
+        var opcao = $('[name="opcao"]:checked').val();
+
+        ESCOPO.confirmarFinal = "pin";
+
+
+        ESCOPO.callback = this.setTimeout;
+        ESCOPO.parametro = this;
+        ESCOPO.dadosOperacao = {
+            "tempo_bloqueio": opcao
+        };
+
+        InicioRequests.pedirNumeroOuPin();
+    }
+    setTimeout(esse) {
+        esse.loader.abrir();
+        var settings = {
+            "url": (esse.apiUrl)+"/config/settimeout",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "token": db.getToken(),
+                "pin": ESCOPO.pin,
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify(ESCOPO.dadosOperacao),
+        };
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            if(response.ok){
+                InicioRequests.home();
+                ESCOPO.modalConfirmarFinal.hide();
+                esse.notificacao.sms(response.payload, 0);
+                setTimeout(function(){
+                    vaiTela("\home");
+                },1000);
+            }else{
+                ESCOPO.modalConfirmarFinal.hide();
+                $("#codigo-confirmacao").val("");
+                esse.notificacao.sms(response.payload, 1);
+                esse.loader.fechar();
+            }
+        });
+    }
+
+      alterarPin() {
+        var pin_novo = $("#pin_novo").val();
+        var pin_novo_confirmar = $("#pin_novo_confirmar").val();
+
+        if(pin_novo.length != 6){
+            this.notificacao.sms("Verifica o novo PIN, deve ter 6 digitos", 1);
+            return;
+        }
+        if(pin_novo != pin_novo_confirmar){
+            this.notificacao.sms("Verifica o novo PIN, as combinaçoes devem ser iguais", 1);
+            return;
+        }
+        ESCOPO.confirmarFinal = "pin";
+        ESCOPO.callback = this.setPin;
+        ESCOPO.parametro = this;
+        ESCOPO.dadosOperacao = {
+            "pin": pin_novo
+        };
+
+        InicioRequests.pedirNumeroOuPin();
+    }
+    setPin(esse) {
+        esse.loader.abrir();
+        var settings = {
+            "url": (esse.apiUrl)+"/config/alterarpin",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "token": db.getToken(),
+                "pin": ESCOPO.pin,
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify(ESCOPO.dadosOperacao),
+        };
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            if(response.ok){
+                InicioRequests.home();
+                ESCOPO.modalConfirmarFinal.hide();
+                esse.notificacao.sms(response.payload, 0);
+                setTimeout(function(){
+                    vaiTela("\home");
+                },1000);
+            }else{
+                ESCOPO.modalConfirmarFinal.hide();
+                $("#codigo-confirmacao").val("");
+                esse.notificacao.sms(response.payload, 1);
+                esse.loader.fechar();
+            }
+        });
     }
 }
