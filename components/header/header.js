d51950333d57e023ba10d7845836d2bb35430c48
cbcaetanoborges <cbcaetanoborges@gmail.com>
2024-11-04 13:34:48 +0100

    deu

--- components/header/header.js
+++ components/header/header.js
@@ -11,7 +11,7 @@ debliwui_top.innerHTML = `
 
     <div class="top-notification">
         <img src="assets/notificacao-icon.svg" class="notification-icon">
-        <p class="label">Olá<br><span>Super Junior</span></p>
+        <p class="label">Olá<br><span></span></p>
         <img src="assets/user.svg" class="user-icon" onclick='vaiTela("perfil")'>
     </div>
 `;
